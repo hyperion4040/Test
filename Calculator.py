e8e6766c51b97c69be9f980969b4659f9c4448ad
hyperion4040 <hyperion1987@wp.pl>
2018-11-12 18:31:48 +0100

    Initial commit

---
+++ Calculator.py
@@ -0,0 +1,6 @@
+def add_values(a,b):
+    return a+b
+
+
+result = add_values(2,2)
+print(result)
---
+++ test_add_values.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+from Calculator import add_values
+
+class TestAdd_values(TestCase):
+    def test_add_values(self):
+        assert 4== add_values(2,2)
